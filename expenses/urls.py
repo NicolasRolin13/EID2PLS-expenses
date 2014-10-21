61d8ac6434d902474f4d322db449f581af1617bb
Lucas Serrano <serrano@crans.org>
2014-10-21 15:14:25 +0200

    [expenses] Added user authentification

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
-import expenses.views
+from expenses import views
 
 urlpatterns = patterns('',
-            url(r'^$', expenses.views.normal_bill_form),
+            url(r'^$', views.normal_bill_form),
+            url(r'^accounts/login/$', views.view_login, name='view_login'),
+            url(r'^accounts/login/fail/$', views.view_login, {'error' : 1}, name='view_login_fail'),
+            url(r'^accounts/do_login/$', views.do_login),
+            url(r'^accounts/logout/$', views.view_logout),
             )
--- expenses/views.py
+++ expenses/views.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.debug import sensitive_post_parameters
+
 from expenses.forms import BillForm
 
+@login_required
 def normal_bill_form(request):
     if request.POST:
         submitted_form = BillForm(request.POST)
@@ -14,3 +19,37 @@ def normal_bill_form(request):
             bill_model.save()
             render(request, 'thanks.html')
     return render(request, 'index.html', {'form':BillForm(request.POST)})
+
+
+# Login handling
+#################
+
+@login_required
+def view_logout(request):
+    logout(request)
+    return render(request, 'logout.html')
+
+def view_login(request, error=None):
+    if 'next' in request.GET:
+        request.session['next_page'] = request.GET['next']
+    return render(request, 'login.html', {'error' : error})
+
+@sensitive_post_parameters('password')
+def do_login(request):
+    params = {'username' : '', 'password' : '', 'next_page' : '/'}
+
+    for (key, value) in request.POST.items():
+        params[key] = value
+
+    if 'next_page' in request.session:
+        if request.session['next_page']:
+            params['next_page'] = request.session.pop('next_page')
+
+    user = authenticate(username=params['username'], password=params['password'])
+    if user is not None:
+        login(request, user)
+        return redirect(params['next_page'])
+    else:
+        request.session['next_page'] = params['next_page']
+        return redirect('view_login_fail')
+
