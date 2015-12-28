89ac7f48555ab78a04836a5b44cb2a04934af742
Lucas Serrano <serrano@crans.org>
2015-12-28 12:24:35 +0100

    Fix: admin js not included for non staff users

--- Share/urls.py
+++ Share/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.views.i18n import javascript_catalog
+
 urlpatterns = [
+    url(r'^admin/jsi18n/$', javascript_catalog),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('expenses.urls')),
 ]
