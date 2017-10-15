0891bbbc5ceba48ac5faa74dfa487f27b27f79cc
trolin <nicolasjrolin@gmail.com>
2017-10-15 17:26:55 +0200

    this commit will fail due to flake8

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     name='expenses',
     version='0.1',
     description=('Does pretty much nothing'),
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
     packages=find_packages(),
     entry_points={'console_scripts': [
         'mon_entry_point = expenses.core:does_nothing',
